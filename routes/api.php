63a74a4cfe20fe860b66e3dfda32e688bdbc2217
kyawzinwin2002 <kyawzinw469@gmail.com>
2024-09-22 16:40:22 +0700

    feat: add route for post and comment

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\v1\Auth\LoginController;
 use App\Http\Controllers\Api\v1\Auth\RegisterController;
+use App\Http\Controllers\Api\v1\CommentController;
+use App\Http\Controllers\Api\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +14,19 @@ Route::get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::post('/register', [RegisterController::class, 'register']);
     Route::post('/login', [LoginController::class, 'login']);
+
+    Route::group(function () {
+        Route::get('/posts', [PostController::class, 'index']);
+        Route::get('/posts/{post}', [PostController::class, 'show']);
+        Route::post('/posts', [PostController::class, 'create']);
+        Route::put('/posts/{post}', [PostController::class, 'update']);
+        Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+        Route::get('/posts/{post}/comments', [CommentController::class, 'index']);
+        Route::get('/comments/{comment}', [CommentController::class, 'show']);
+        Route::post('/comments', [CommentController::class, 'create']);
+        Route::put('/comments/{comment}', [CommentController::class, 'update']);
+        Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+    })->middleware('auth:sanctum');
 });
+
